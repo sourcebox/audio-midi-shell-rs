a76bff0384cb28123c4e85a7f6e8afa4996abe98
Oliver Rockstedt <info@sourcebox.de>
2023-05-20 18:52:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# audio-midi-shell
+
+Cross-platform wrapper around [tinyaudio](https://crates.io/crates/tinyaudio) and [midir](https://crates.io/crates/midir) for prototyping audio algorithms as standalone applications.
+
+It opens the default output device with a given sample rate and process block size as well as all MIDI input ports found.
+
+## Usage
+
+```rust
+use audio_midi_shell::{AudioMidiShell, AudioGenerator};
+
+const SAMPLE_RATE: u32 = 44100;
+const BLOCK_SIZE: usize = 256;
+
+fn main() -> ! {
+    AudioMidiShell::run_forever(SAMPLE_RATE, BLOCK_SIZE, TestGenerator);
+}
+
+struct TestGenerator;
+
+impl AudioGenerator for TestGenerator {
+    fn init(&mut self, block_size: usize) {
+        // Optional function, called once on startup for initialization tasks.
+    }
+
+    fn process(&mut self, samples_left: &mut [f32], samples_right: &mut [f32]) {
+        // Called periodically with buffers of `BLOCK_SIZE` samples.
+        // Fill `samples_left` and `samples_right` with audio data accordingly.
+    }
+
+    fn process_midi(&mut self, message: Vec<u8>) {
+        // Optional function, called on each incoming MIDI message.
+    }
+}
+
+```
+
+## License
+
+Published under the MIT license. Any contribution to this project must be provided under the same license conditions.
+
+Author: Oliver Rockstedt <info@sourcebox.de>
---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
+
+use std::sync::mpsc;
+
+use midir::{MidiInput, MidiInputConnection};
+use tinyaudio::{run_output_device, OutputDeviceParameters};
+
+/// Shell running the audio and MIDI processing.
+pub struct AudioMidiShell;
+
+impl AudioMidiShell {
+    /// Initializes the output device and runs the generator in an infinite loop.
+    /// - `sample_rate` is the sampling frequency in Hz.
+    /// - `block_size` is the number of samples for the `process` function.
+    pub fn run_forever(
+        sample_rate: u32,
+        block_size: usize,
+        mut generator: impl AudioGenerator + Send + 'static,
+    ) -> ! {
+        let (midi_sender, midi_receiver): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) =
+            mpsc::channel();
+        let _midi_connections = init_midi(midi_sender);
+
+        generator.init(block_size);
+
+        let params = OutputDeviceParameters {
+            channels_count: 2,
+            sample_rate: sample_rate as usize,
+            channel_sample_count: block_size,
+        };
+
+        let _device = run_output_device(params, move |data| {
+            while let Ok(message) = midi_receiver.try_recv() {
+                generator.process_midi(message);
+            }
+
+            let mut samples_left = vec![0.0; block_size];
+            let mut samples_right = vec![0.0; block_size];
+            generator.process(&mut samples_left, &mut samples_right);
+
+            for (frame_no, samples) in data.chunks_mut(params.channels_count).enumerate() {
+                samples[0] = samples_left[frame_no];
+                samples[1] = samples_right[frame_no];
+            }
+        })
+        .unwrap();
+
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+}
+
+/// Trait to be implemented by structs that are passed as generator to the shell.
+pub trait AudioGenerator {
+    /// Initializes the generator. Called once inside the shell `run` function.
+    fn init(&mut self, _block_size: usize) {}
+
+    /// Generates a block of samples.
+    /// `samples_left` and `samples_right` are buffers of the block size passed to the shell `run`
+    /// function. They are initialized to `0.0` and must be filled with sample data.
+    fn process(&mut self, samples_left: &mut [f32], samples_right: &mut [f32]);
+
+    /// Processes a MIDI message.
+    fn process_midi(&mut self, _message: Vec<u8>) {}
+}
+
+/// Vector of MIDI connections with an attached mpsc sender.
+type MidiConnections = Vec<MidiInputConnection<mpsc::Sender<Vec<u8>>>>;
+
+/// Connects all available MIDI inputs to an mpsc sender and returns them in a vector.
+fn init_midi(sender: mpsc::Sender<Vec<u8>>) -> MidiConnections {
+    let mut connections = MidiConnections::new();
+
+    let input = MidiInput::new(&(env!("CARGO_PKG_NAME").to_owned() + " scan input"))
+        .expect("MIDI Input error");
+
+    for port in input.ports().iter() {
+        let input = MidiInput::new(&(env!("CARGO_PKG_NAME").to_owned() + " input"))
+            .expect("MIDI Input error");
+        let port_name = input.port_name(port).unwrap();
+        log::info!("Connecting to MIDI input {}", port_name);
+        let conn = input
+            .connect(
+                port,
+                port_name.as_str(),
+                |_timestamp, message, sender| {
+                    sender.send(Vec::from(message)).ok();
+                },
+                sender.clone(),
+            )
+            .ok();
+        connections.push(conn.unwrap());
+    }
+
+    connections
+}
